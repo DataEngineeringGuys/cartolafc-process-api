0d1790eea5d8d986e6048a1f217390a50349acf3
Yuri Fialho <yuri.fialho@yandeh.com>
2018-04-10 10:16:24 -0300

    restructured project

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 
-df_atletas = pd.read_csv(filepath_or_buffer='./cartolafc-extract/data/atletas.csv')
-df_partidas = pd.read_csv(filepath_or_buffer='./cartolafc-extract/data/partidas.csv')
-df_clubes = pd.read_csv(filepath_or_buffer='./cartolafc-extract/data/clubes.csv')
+df_atletas = pd.read_csv(filepath_or_buffer='./extract/data/atletas.csv')
+df_partidas = pd.read_csv(filepath_or_buffer='./extract/data/partidas.csv')
+df_clubes = pd.read_csv(filepath_or_buffer='./extract/data/clubes.csv')
 
 df_atletas_clube = pd.merge(df_atletas, df_clubes, how='inner', left_on=['clube_id'], right_on=['id'])
 
